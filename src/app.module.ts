f98bfa0b198e4cf018ab2b4b97bfaf35ade77cb9
fazliddinofficial <fazliddinquvatboyev8@gmail.com>
2025-04-24 11:58:48 +0500

    add: user schema and mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { BookModule } from './book/book.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/library'),
+    UserModule, BookModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
