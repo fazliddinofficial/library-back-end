cde0e424ef7783d426c9e369eb278b67fe35c778
fazliddinofficial <fazliddinquvatboyev8@gmail.com>
2025-04-24 16:48:23 +0500

    set up basic schema for models

--- src/book/entities/book.entity.ts
+++ src/book/entities/book.entity.ts
@@ -1,1 +1,12 @@
-export class Book {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type BookDocument = HydratedDocument<Book>;
+
+@Schema()
+export class Book {
+  @Prop({ type: String })
+  name: string;
+}
+
+export const BookSchema = SchemaFactory.createForClass(Book);
